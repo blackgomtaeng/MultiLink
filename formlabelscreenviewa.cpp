2cdf900ad12190a58a9ef5f90765e982efcc2baf
blackgomtaeng <12zz12@kakao.com>
2026-07-19 20:16:01 +0900

    20260719 전방 선언 + PIMPL 패턴을 쓰는 이유
    전방 선언 + PIMPL 패턴을 쓰는 이유
    01 경고 제거
    02 불필요한 의존성 차단
    03 빌드 속도 개선
    04 유지보수성과 캡슐화 강화
    05 ABI 안정성 확보
    
    https://discordapp.com/channels/1425417432188846202/1434777303275016292 해당하는 내용들이 담겨 있으므로 여기에 적합한 내용으로 적용하여 만들어주기를 원한다.
    
    탭 관리, 옵션·닫기 메뉴, PIMPL로 내부 구현 숨김

--- formlabelscreenviewa.cpp
+++ formlabelscreenviewa.cpp
@@ -1,47 +1,94 @@
 #include "formlabelscreenviewa.h"
 #include "ui_formlabelscreenviewa.h"
+#include "stackedwidgettaboptions.h"   // 여기서만 include
 
-FormLabelScreenViewA::FormLabelScreenViewA(QWidget *parent):
-    QWidget(parent),
-    ui(new Ui::FormLabelScreenViewA),
-    tabs(new TabStruct)
+struct FormLabelScreenViewA::Impl {
+    Ui::FormLabelScreenViewA* ui;
+    TabStruct* tabs;
+    BaseTypes bt;
+    Impl(): ui(new Ui::FormLabelScreenViewA), tabs(new TabStruct) {}
+    ~Impl() { delete tabs->plusIndex; delete tabs; delete ui; }
+};
+
+FormLabelScreenViewA::FormLabelScreenViewA(QWidget* parent)
+    : QWidget(parent), d(new Impl)
 {
-    ui->setupUi(this);
-    tabs->tabWidget = ui->tabWidget;
+    d->ui->setupUi(this);
+    d->tabs->tabWidget = d->ui->tabWidget;
+    d->tabs->tabWidget->clear();
+    d->tabs->tabWidget->setTabsClosable(true);
 
-    tabs->tabWidget->clear();
-    tabs->tabWidget->setTabsClosable(true);
+    d->tabs->newTab = new QWidget;
+    d->tabs->tabWidget->addTab(d->tabs->newTab, "New Tab");
 
-    tabs->newTab = new QWidget;
-    tabs->tabWidget->addTab(tabs->newTab, "New Tab");
+    d->tabs->plusTab = new QWidget;
+    d->tabs->tabWidget->addTab(d->tabs->plusTab, QIcon(":/mlPlus.png"), "");
 
-    tabs->plusTab = new QWidget;
-    tabs->tabWidget->addTab(tabs->plusTab, QIcon(":/mlPlus.png"), "");
+    d->tabs->plusIndex = new int(d->tabs->tabWidget->count() - 1);
+    d->tabs->tabWidget->tabBar()->setTabButton(*d->tabs->plusIndex, QTabBar::RightSide, nullptr);
 
-    tabs->plusIndex = tabs->tabWidget->count() - 1;
-    tabs->tabWidget->tabBar()->setTabButton(tabs->plusIndex, QTabBar::RightSide, nullptr);
+    d->tabs->contextMenu = new QMenu(d->tabs->tabWidget);
+    d->tabs->actionOptions = new QAction("Tab Options", d->tabs->contextMenu);
+    d->tabs->actionClose = new QAction("Close Tab", d->tabs->contextMenu);
+    d->tabs->contextMenu->addAction(d->tabs->actionOptions);
+    d->tabs->contextMenu->addAction(d->tabs->actionClose);
 
-    connect(tabs->tabWidget, &QTabWidget::tabBarClicked, this, &FormLabelScreenViewA::handleClickedTab);
-    connect(tabs->tabWidget, &QTabWidget::tabCloseRequested, this, &FormLabelScreenViewA::handleTabCloseRequested);
+    connect(d->tabs->tabWidget, &QTabWidget::tabBarClicked, this, &FormLabelScreenViewA::handleClickedTab);
+    connect(d->tabs->tabWidget, &QTabWidget::tabCloseRequested, this, &FormLabelScreenViewA::handleTabCloseRequested);
+    connect(d->tabs->tabWidget->tabBar(), &QTabBar::customContextMenuRequested, this, &FormLabelScreenViewA::showContextMenu);
+    d->tabs->tabWidget->tabBar()->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    connect(d->tabs->actionOptions, &QAction::triggered, this, &FormLabelScreenViewA::handleOptions);
+    connect(d->tabs->actionClose, &QAction::triggered, this, &FormLabelScreenViewA::handleClose);
 }
 
-FormLabelScreenViewA::~FormLabelScreenViewA() {
-    delete tabs;
-    delete ui;
-}
+FormLabelScreenViewA::~FormLabelScreenViewA() { delete d; }
 
 void FormLabelScreenViewA::handleClickedTab(int index) {
-    if (index == tabs->tabWidget->count() - 1) {
-        QWidget *newTab = new QWidget;
-        tabs->tabWidget->insertTab(tabs->tabWidget->count() - 1, newTab, "New Tab");
-        tabs->tabWidget->setCurrentIndex(tabs->tabWidget->count() - 2);
+    if(index == d->tabs->tabWidget->count() - 1) {
+        QWidget* newTab = new QWidget;
+        d->tabs->tabWidget->insertTab(d->tabs->tabWidget->count() - 1, newTab, "New Tab");
+        d->tabs->tabWidget->setCurrentIndex(d->tabs->tabWidget->count() - 2);
     }
 }
 
 void FormLabelScreenViewA::handleTabCloseRequested(int index) {
-    if (index != tabs->tabWidget->count() - 1) {
-        tabs->widget = tabs->tabWidget->widget(index);
-        tabs->tabWidget->removeTab(index);
-        delete tabs->widget;
+    if(index != d->tabs->tabWidget->count() - 1) {
+        d->tabs->widget = d->tabs->tabWidget->widget(index);
+        d->tabs->tabWidget->removeTab(index);
+        delete d->tabs->widget;
+    }
+}
+
+void FormLabelScreenViewA::showContextMenu(const QPoint& pos) {
+    d->bt.index = d->tabs->tabWidget->tabBar()->tabAt(pos);
+    if(d->bt.index >= 0 || d->bt.index == *d->tabs->plusIndex)
+        d->tabs->contextMenu->exec(d->tabs->tabWidget->tabBar()->mapToGlobal(pos));
+}
+
+void FormLabelScreenViewA::handleOptions() {
+    d->bt.count = d->tabs->tabWidget->count();
+    d->bt.plusIdx = *d->tabs->plusIndex;
+    d->bt.exists = false;
+    for(int i = 0; i < d->bt.count; ++i) {
+        if(d->tabs->tabWidget->tabText(i) == "Tab Options") {
+            d->bt.exists = true;
+            d->tabs->tabWidget->setCurrentIndex(i);
+            break;
+        }
+    }
+    if(!d->bt.exists) {
+        auto* optionsWidget = new StackedWidgetTabOptions; // 반드시 QWidget 상속 필요
+        d->tabs->tabWidget->insertTab(d->bt.plusIdx, optionsWidget, "Tab Options");
+        d->tabs->tabWidget->setCurrentWidget(optionsWidget);
+    }
+}
+
+void FormLabelScreenViewA::handleClose() {
+    d->bt.index = d->tabs->tabWidget->currentIndex();
+    if(d->bt.index != d->tabs->tabWidget->count() - 1) {
+        d->tabs->widget = d->tabs->tabWidget->widget(d->bt.index);
+        d->tabs->tabWidget->removeTab(d->bt.index);
+        delete d->tabs->widget;
     }
 }
--- formlabelscreenviewa.h
+++ formlabelscreenviewa.h
@@ -5,30 +5,47 @@
 #include <QTabWidget>
 #include <QTabBar>
 #include <QIcon>
+#include <QMenu>
+#include <QAction>
 
 namespace Ui { class FormLabelScreenViewA; }
 
+class StackedWidgetTabOptions;  // 전방 선언
+
 typedef struct {
-    QTabWidget *tabWidget;
-    QWidget *newTab;
-    QWidget *plusTab;
-    QWidget *widget;
-    int plusIndex;
+    QTabWidget* tabWidget;
+    QWidget* newTab;
+    QWidget* plusTab;
+    QWidget* widget;
+    QMenu* contextMenu;
+    QAction* actionOptions;
+    QAction* actionClose;
+    int* plusIndex;
 } TabStruct;
+
+typedef struct {
+    int index;
+    int count;
+    int plusIdx;
+    bool exists;
+} BaseTypes;
 
 class FormLabelScreenViewA : public QWidget {
     Q_OBJECT
 public:
-    explicit FormLabelScreenViewA(QWidget *parent = nullptr);
+    explicit FormLabelScreenViewA(QWidget* parent = nullptr);
     ~FormLabelScreenViewA();
 
 private slots:
     void handleClickedTab(int index);
     void handleTabCloseRequested(int index);
+    void showContextMenu(const QPoint& pos);
+    void handleOptions();
+    void handleClose();
 
 private:
-    Ui::FormLabelScreenViewA *ui;
-    TabStruct *tabs;
+    struct Impl;   // PIMPL 구조체 선언
+    Impl* d;       // 구현 포인터
 };
 
-#endif // FORMLABELSCREENVIEWA_H
+#endif
